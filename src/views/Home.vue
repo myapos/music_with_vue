55552c9eec63ebe66fffecc568629fc1e8901c23
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-08-20 20:28:37 +0300

    directives

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -29,12 +29,13 @@
       <div
         class="bg-white rounded border border-gray-200 relative flex flex-col"
       >
-        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+        <!-- prettier-ignore -->
+        <div
+          class="px-6 pt-6 pb-5 font-bold border-b border-gray-200"
+          v-icon-secondary="{ icon: 'headphones-alt', right: true }"
+        >
           <span class="card-title">Songs</span>
           <!-- Icon -->
-          <i
-            class="fa fa-headphones-alt float-right text-green-400 text-xl"
-          ></i>
         </div>
         <!-- Playlist -->
         <ol id="playlist"></ol>
@@ -48,6 +49,8 @@
 <script>
 import { songsCollection } from "@/includes/firebase";
 import AppSongItem from "@/components/SongItem.vue";
+import IconSecondary from "@/directives/icon-secondary";
+
 // @ is an alias to /src
 
 export default {
@@ -113,5 +116,8 @@ export default {
     },
   },
   components: { AppSongItem },
+  directives: {
+    "icon-secondary": IconSecondary,
+  },
 };
 </script>
