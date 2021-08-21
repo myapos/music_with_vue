005b2391d2317743995cabb0852b20476c377991
Myron Apostolakis <myron.apostolakis@sunlight.io>
2021-08-21 13:59:29 +0300

    pwa

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,11 @@ module.exports = {
       fullInstall: true,
     },
   },
+  pwa: {
+    name: "Music App",
+    themColor: "#ff5e3a",
+    manifestOptions: {
+      short_name: "Music",
+    },
+  },
 };
